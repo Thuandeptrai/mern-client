2f484ecd9c5736507f9578876c5d830ef0b482ad
Thuandeptrai <tvc4567@gmail.com>
2021-05-15 16:04:14 +0700

    bad

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://blooming-lowlands-14678.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
